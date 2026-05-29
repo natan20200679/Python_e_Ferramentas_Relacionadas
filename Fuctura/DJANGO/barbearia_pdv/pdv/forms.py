84b4d9f4e5b0829bc953db9b9fb7d394b68b13aa
natan_bitbucket <natanaelmedrado@outlook.com>
2026-05-29 15:28:40 -0300

    Create UserRegistrationForm

--- Fuctura/DJANGO/barbearia_pdv/pdv/forms.py
+++ Fuctura/DJANGO/barbearia_pdv/pdv/forms.py
@@ -22,3 +22,15 @@ class AgendamentoForm(forms.ModelForm):
                 'type': 'date'
             }),
         }
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        if (password and password_confirm) and (password != password_confirm):
+            raise forms.ValidationError("As senhas não coincidem!")
