afee468fa841cf7a2c4397d07c7bd9ea9eb056e1
natan_bitbucket <natanaelmedrado@outlook.com>
2026-06-01 11:14:25 -0300

    Create UserRegistrationForm

--- Fuctura/Biblioteca/registros/forms.py
+++ Fuctura/Biblioteca/registros/forms.py
@@ -25,3 +25,15 @@ class EmprestimoForm(forms.ModelForm):
                 'placeholder': 'DD/MM/AAAA',
                 'type': 'date'
         })}
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        if (password and password_confirm) and (password != password_confirm):
+            raise forms.ValidationError("As senhas não coincidem!")
