c5cee669e53be95e9ff2daaf9fe60f3cca377784
natan_bitbucket <natanaelmedrado@outlook.com>
2026-06-05 20:50:26 -0300

    Delete Projects

--- Fuctura/Django/barbearia_pdv/pdv/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Cliente, Servico, Agendamento
-
-# Registre os modelos para que apareçam no painel de administração
-admin.site.register(Cliente)
-admin.site.register(Servico)
-admin.site.register(Agendamento)
-
-
--- Fuctura/DjangoProject/biblioteca/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from biblioteca.models import Livro, Usuario, Emprestimo
-
-admin.site.register(Livro)
-admin.site.register(Usuario)
-admin.site.register(Emprestimo)
